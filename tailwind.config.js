3e941067aec8c53dbb68e99b0ec756e129afdfa7
elciddelapaz <elcid.delapaz@gmail.com>
2023-07-24 16:26:38 +0800

    Revert "added animation"
    This reverts commit d394be6fe786b90defbb5652930bf6c96f82fc9c.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {
-      animation: {
-        text: "text 5s ease infinite",
-      },
-      keyframes: {
-        text: {
-          "0%, 100%": {
-            "background-size": "300% 300%",
-            "background-position": "left center",
-          },
-          "50%": {
-            "background-size": "300% 300%",
-            "background-position": "right center",
-          },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
