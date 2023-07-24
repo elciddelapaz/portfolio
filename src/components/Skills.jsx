59ff848255897e009317a9e177f99f1d3bdc45b5
elciddelapaz <elcid.delapaz@gmail.com>
2023-07-24 17:09:54 +0800

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,15 +14,13 @@ import Vue from "../assets/vue.png";
 export default function Skills() {
   return (
     <div name="skills" className="w-full h-screen bg-black text-gray-300">
-      {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-rose-600 ">
             Skills
           </p>
         </div>
-
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+        <div className="w-full grid grid-cols-3 sm:grid-cols-5 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-100">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
             <p className="my-4">HTML</p>
