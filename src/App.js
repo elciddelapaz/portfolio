72d2492a9cc276e979e0c51ea200a0ef4e4997f7
elciddelapaz <elcid.delapaz@gmail.com>
2023-07-25 10:27:40 +0800

    add footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Skills />
       <Work />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import { FaLinkedin, FaFacebookF } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+
+export default function Footer() {
+  return (
+    <footer className="bg-black">
+      <div className="flex justify-center text-rose-50">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <hr className="my-6 border-rose-800 sm:mx-auto lg:my-8" />
+          <div className="flex flex-col items-center gap-4">
+            <h1 className="font-bold text-xl">Socials</h1>
+            <div className="flex gap-4 mb-4">
+              <a href="https://www.facebook.com/elcidlang/">
+                <FaFacebookF size={30} />
+              </a>
+              <a href="https://www.linkedin.com/in/elcid-dela-paz-91a9a6202">
+                <FaLinkedin size={30} />
+              </a>
+              <a href="mailto:elcid.delapaz@gmail.com">
+                <FiMail size={30} />
+              </a>
+            </div>
+          </div>
+          <span className="block text-sm sm:text-center text-justify">
+            Copyright © 2023. All Rights Reserved. This portfolio is made using
+            ReactJS and Tailwind CSS.
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+}
