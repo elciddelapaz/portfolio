aaff4d12934738af9d7a133dd0c27f021dd3bb6f
elciddelapaz <elcid.delapaz@gmail.com>
2024-02-28 12:01:04 +0800

    update: skills icon

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,6 +10,10 @@ import Mongo from "../assets/mongo.png";
 import Python from "../assets/python.png";
 import Tailwind from "../assets/tailwind.png";
 import Vue from "../assets/vue.png";
+import Laravel from "../assets/laravel.png";
+import PHP from "../assets/php.png";
+import MySQL from "../assets/mysql.png";
+import Git from "../assets/git.png";
 
 export default function Skills() {
   return (
@@ -38,8 +42,16 @@ export default function Skills() {
             <p className="my-4">JavaScript</p>
           </div>
           <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-75">
+            <img className="w-20 mx-auto" src={PHP} alt="PHP icon" />
+            <p className="my-4">PHP</p>
+          </div>
+          <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-75">
             <img className="w-20 mx-auto" src={Node} alt="Node icon" />
             <p className="my-4">Node JS</p>
+          </div>
+          <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-75">
+            <img className="w-20 mx-auto" src={Laravel} alt="Laravel icon" />
+            <p className="my-4">Laravel</p>
           </div>
           <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-75">
             <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
@@ -50,12 +62,20 @@ export default function Skills() {
             <p className="my-4">Vue</p>
           </div>
           <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-75">
+            <img className="w-20 mx-auto" src={Git} alt="Git icon" />
+            <p className="my-4">Git</p>
+          </div>
+          <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-75">
             <img className="w-20 mx-auto" src={GitHub} alt="GitHub icon" />
             <p className="my-4">GitHub</p>
           </div>
           <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-75">
             <img className="w-20 mx-auto" src={Mongo} alt="Mongo icon" />
             <p className="my-4">Mongo DB</p>
+          </div>
+          <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-75">
+            <img className="w-20 mx-auto" src={MySQL} alt="MySQL icon" />
+            <p className="my-4">MySQL</p>
           </div>
           <div className="shadow-md shadow-[#4c0519] hover:scale-110 duration-75">
             <img className="w-20 mx-auto" src={Python} alt="Python icon" />
